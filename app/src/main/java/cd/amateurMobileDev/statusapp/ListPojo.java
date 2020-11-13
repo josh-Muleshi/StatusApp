f455838e964f76caafcaae61f4b7b8aa1b4ba7da
josh muleshi <jmuleshi2@gmail.com>
2020-11-13 15:30:48 +0000

    add of attributes, constrictor and getter in the ListPojo class

--- app/src/main/java/cd/amateurMobileDev/statusapp/ListPojo.java
+++ app/src/main/java/cd/amateurMobileDev/statusapp/ListPojo.java
@@ -1,4 +1,26 @@
 package cd.amateurMobileDev.statusapp;
 
 public class ListPojo {
+
+    private String title;
+    private String description;
+    private int images;
+
+    public ListPojo(String title, String description, int images) {
+        this.title = title;
+        this.description = description;
+        this.images = images;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getImages() {
+        return images;
+    }
 }
